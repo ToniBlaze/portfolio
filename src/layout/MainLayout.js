004e2cfa6da05337fd80d03d34518f9ac1aa695f
Alessio <alessio.toninello@gmail.com>
2023-12-14 21:47:02 +0100

    create footer components

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import MyNav from '../components/MainLayoutComponents/MyNav'
+import Footer from '../components/MainLayoutComponents/Footer'
 
 export default function MainLayout({children}) {
   return (
     <>
-    {/* <MyNav /> */}
+    <MyNav />
     {children}
-    {/* <Footer /> */}
+    <Footer />
     </>
   )
 }
