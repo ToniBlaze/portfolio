452b7bd40964ee48bfd82bc38f7b209b90d3811c
Alessio <alessio.toninello@gmail.com>
2023-12-18 21:26:49 +0100

    adding section placeholder

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,6 +10,9 @@ export default function Homepage() {
         <Row>
         {/* ELEMENTS */}
         <h1>Hi! I'm Alessio!</h1>
+        <h3>Who i am</h3>
+        <h3>Project</h3>
+        <h3>Contact</h3>
         </Row>
       </Container>
     </MainLayout>
