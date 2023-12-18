02e3444b5257242676f664548ecf3954198baa50
Alessio <alessio.toninello@gmail.com>
2023-12-18 21:26:06 +0100

    css footer text

--- src/components/MainLayoutComponents/Footer.js
+++ src/components/MainLayoutComponents/Footer.js
@@ -4,25 +4,25 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Footer() {
   return (
     <Container fluid>
-    <Row className="d-flex flex-wrap justify-content-around align-items-center pb-5 pt-4 border-top bg-black">
-      <Col md={4} className="mb-0 text-body-secondary">
+    <Row className="d-flex flex-wrap justify-content-around align-items-center pb-5 pt-4 border-top bg-black text-secondary">
+      <Col md={4} className="mb-0 text-secondary">
         © 2023 Company, Inc
       </Col>
 
       <Col md={4} className="d-flex justify-content-center">
         <ul className="nav">
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
+            <a href="#" className="nav-link px-2 text-secondary">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
+            <a href="#" className="nav-link px-2 text-secondary">
               Project
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
+            <a href="#" className="nav-link px-2 text-secondary">
               Contact
             </a>
           </li>
