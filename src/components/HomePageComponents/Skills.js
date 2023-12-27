e332e4e92b61a3ad19ad8d319f528e79b65415f4
Alessio <alessio.toninello@gmail.com>
2023-12-27 21:00:47 +0100

    add fontawesome

--- src/components/HomePageComponents/Skills.js
+++ src/components/HomePageComponents/Skills.js
@@ -6,13 +6,38 @@ export default function Skills() {
     <Col id="project" className="py-5 mb-5 col-12">
       <Col className="d-flex justify-content-center text-center col-12">
         <Col xs={10} md={8}>
-          <h2 className="pb-2 border-bottom"> Skills </h2> 
+          <h2 className="pb-2 border-bottom"> Skills </h2>
         </Col>
-{/* 
-        Fare sezione con PROGRESS BAR che dimostrano le tue skills 
+
+        <Col>
+          <Col>
+            <i class="fa-brands fa-react"></i>
+          </Col>
+
+          <Col>
+            <i class="fa-brands fa-js" ></i>
+          </Col>
+
+          <Col>
+            <i class="fa-brands fa-node" ></i>
+          </Col>
+
+          <Col>
+          </Col>
+
+          <Col>
+          </Col>
+
+          <Col>
+          </Col>
+
+          <Col>
+          </Col>
+        </Col>
+
+        {/*  Fare sezione con PROGRESS BAR che dimostrano le tue skills 
         Oppure fare semplicemente linguaggi e tools o framework che utilizzi */}
       </Col>
-      
-    </Col> 
+    </Col>
   );
 }
