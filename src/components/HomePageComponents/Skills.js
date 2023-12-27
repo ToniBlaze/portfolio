9a34dc3012278c6c6e55e5d959c4dc9b12ac1f4a
Alessio <alessio.toninello@gmail.com>
2023-12-27 20:46:25 +0100

    bug fix

--- src/components/HomePageComponents/Skills.js
+++ src/components/HomePageComponents/Skills.js
@@ -6,7 +6,7 @@ export default function Skills() {
     <Col id="project" className="py-5 mb-5 col-12">
       <Col className="d-flex justify-content-center text-center col-12">
         <Col xs={10} md={8}>
-          <h2 className="pb-2 border-bottom"> Skills </h2>
+          <h2 className="pb-2 border-bottom"> Skills </h2> 
         </Col>
 {/* 
         Fare sezione con PROGRESS BAR che dimostrano le tue skills 
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'remixicon/fonts/remixicon.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
