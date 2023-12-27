2df6f8d01581c4d95734b6b784d9e4d96584282d
Alessio <alessio.toninello@gmail.com>
2023-12-27 21:25:09 +0100

    add icons

--- src/components/HomePageComponents/Skills.js
+++ src/components/HomePageComponents/Skills.js
@@ -1,42 +1,40 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Image } from "react-bootstrap";
+import Express from "../../assets/express.png";
+import Mongo from "../../assets/MongoDB.png";
 
 export default function Skills() {
   return (
-    <Col id="project" className="py-5 mb-5 col-12">
+    <Col id="skills" className="py-5 mb-5 col-12">
       <Col className="d-flex justify-content-center text-center col-12">
         <Col xs={10} md={8}>
           <h2 className="pb-2 border-bottom"> Skills </h2>
         </Col>
-
-        <Col>
-          <Col>
-            <i class="fa-brands fa-react"></i>
-          </Col>
-
-          <Col>
-            <i class="fa-brands fa-js" ></i>
-          </Col>
-
-          <Col>
-            <i class="fa-brands fa-node" ></i>
-          </Col>
-
-          <Col>
-          </Col>
-
-          <Col>
-          </Col>
-
-          <Col>
-          </Col>
-
-          <Col>
-          </Col>
+      </Col>
+      <Col className="d-flex justify-content-center text-center col-12">
+        <Col  >
+          <i class="fa-brands fa-react"></i>
         </Col>
 
-        {/*  Fare sezione con PROGRESS BAR che dimostrano le tue skills 
-        Oppure fare semplicemente linguaggi e tools o framework che utilizzi */}
+        <Col>
+          <i class="fa-brands fa-js"></i>
+        </Col>
+
+        <Col>
+          <i class="fa-brands fa-node"></i>
+        </Col>
+
+        <Col>
+          <i class="fa-brands fa-bootstrap"></i>
+        </Col>
+
+        {/*  <Col>
+            <Image src={Express} alt="express" fluid />
+          </Col>
+
+          <Col>
+            <Image src={Mongo} alt="mongo" fluid />
+          </Col> */}
       </Col>
     </Col>
   );
