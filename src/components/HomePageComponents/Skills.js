6a4eca4b3269a5dfc19de1691a1ac753f143f08f
Alessio <alessio.toninello@gmail.com>
2024-01-11 20:57:58 +0100

    add image mongo, express

--- src/components/HomePageComponents/Skills.js
+++ src/components/HomePageComponents/Skills.js
@@ -6,12 +6,15 @@ import Mongo from "../../assets/MongoDB.png";
 export default function Skills() {
   return (
     <Col id="skills" className="py-5 mb-5 col-12">
+
       <Col className="d-flex justify-content-center text-center col-12">
         <Col xs={10} md={8}>
           <h2 className="pb-2 border-bottom"> Skills </h2>
         </Col>
       </Col>
+
       <Col className="d-flex justify-content-center  flex-wrap text-center col-12">
+
         <Col xs={6} md={4} lg={3} className="icon-font">
           <i class="fa-brands fa-react"  ></i>
         </Col>
@@ -28,13 +31,14 @@ export default function Skills() {
           <i class="fa-brands fa-bootstrap"></i>
         </Col>
 
-        {/*  <Col>
-            <Image src={Express} alt="express" fluid />
-          </Col>
+        <Col xs={6} md={4} lg={3}>
+          <Image id="express-logo" src={Express} alt="express" fluid className="img-fluid" />
+        </Col>
 
-          <Col>
-            <Image src={Mongo} alt="mongo" fluid />
-          </Col> */}
+        <Col xs={6} md={4} lg={3}>
+          <Image id="mongo-logo" src={Mongo} alt="mongo-db" fluid className="img-fluid" />
+        </Col> 
+
       </Col>
     </Col>
   );
