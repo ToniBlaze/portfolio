1652e5083d7dff039c0a9c01f9f3c211c3bbee8b
Alessio <alessio.toninello@gmail.com>
2024-01-11 21:26:50 +0100

    fixing bugs css and image

--- src/components/HomePageComponents/Skills.js
+++ src/components/HomePageComponents/Skills.js
@@ -15,27 +15,27 @@ export default function Skills() {
 
       <Col className="d-flex justify-content-center  flex-wrap text-center col-12">
 
-        <Col xs={6} md={4} lg={3} className="icon-font">
+        <Col xs={6} md={4} lg={3} className="icon-font pt-5">
           <i class="fa-brands fa-react"  ></i>
         </Col>
 
-        <Col xs={6} md={4} lg={3} className="icon-font">
+        <Col xs={6} md={4} lg={3} className="icon-font pt-5">
           <i class="fa-brands fa-js"></i>
         </Col>
 
-        <Col xs={6} md={4} lg={3} className="icon-font">
+        <Col xs={6} md={4} lg={3} className="icon-font pt-5">
           <i class="fa-brands fa-node"></i>
         </Col>
 
-        <Col xs={6} md={4} lg={3} className="icon-font">
+        <Col xs={6} md={4} lg={3} className="icon-font pt-5">
           <i class="fa-brands fa-bootstrap"></i>
         </Col>
 
-        <Col xs={6} md={4} lg={3}>
+        <Col xs={6} md={4} lg={3} className="d-flex justify-content-center align-items-center pt-5">
           <Image id="express-logo" src={Express} alt="express" fluid className="img-fluid" />
         </Col>
 
-        <Col xs={6} md={4} lg={3}>
+        <Col xs={6} md={4} lg={3}className="d-flex justify-content-center align-items-center pt-5">
           <Image id="mongo-logo" src={Mongo} alt="mongo-db" fluid className="img-fluid" />
         </Col> 
 
